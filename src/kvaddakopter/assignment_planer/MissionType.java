94db0f2729ae12e38fe46a61d587586e26ed5c62
Per Öberg <per.oberg@cassun.se>
2014-11-06 17:24:11 +0100

    Updated mission object for assignment Planner

--- src/kvaddakopter/assignment_planer/MissionType.java
+++ src/kvaddakopter/assignment_planer/MissionType.java
@@ -1,7 +1,22 @@
 package kvaddakopter.assignment_planer;
 
 public enum MissionType {
-	AROUND_COORDINATE,
-	ALONG_TRAJECTORY,
-	AREA_COVERAGE
+
+
+	AROUND_COORDINATE("Runt Koordinat"),
+	ALONG_TRAJECTORY("Längs bana"),
+	AREA_COVERAGE("Inom område");
+	
+	
+	private String name;
+	
+	private MissionType(String name){
+		this.name = name;
+	}
+	
+	public String toString() {
+		return this.name;
+	}
+
+
 }
