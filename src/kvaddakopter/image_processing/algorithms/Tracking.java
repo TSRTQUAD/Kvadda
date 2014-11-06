c3ce2ad137202d692d35cc92fcbdf3b1c7d77ca7
Magnus Blomberg <magnus.blomberg@telia.com>
2014-11-06 10:11:23 +0100

    Tracking progress. Mostly some calculations.

--- src/kvaddakopter/image_processing/algorithms/Tracking.java
+++ src/kvaddakopter/image_processing/algorithms/Tracking.java
@@ -5,16 +5,51 @@ import java.util.HashMap;
 
 import kvaddakopter.image_processing.data_types.TargetObject;
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
 public class Tracking {
 	
-	HashMap<Integer, TargetObject> mInternalTargets;
+	Mat emptyMat; // For easier use with Core.gemm
 	
+	Mat H_small; 
+	Mat H;
+	
+	HashMap<Integer, TargetObject> mInternalTargets;
+	Mat x; // State matrix containing all targets state matrices
+	Mat P; // Covariance matrix -||-
+	
+	Mat F_small; // Matrix describing the dynamics (constant acceleration)
+	Mat F;
+	Mat G; // Noise dependency
+	Mat Q; // G*G'
+	Mat R; // Measurement noise
+	
+
 	/**
 	 *  <Lägg till beskrivning utav track här>
 	 * 
 	 * @param targetObject - List of detected targets
 	 */
+	
+	public Tracking(){
+		// Initialize matrices
+		emptyMat = new Mat();
+		
+		// Create H_small as [1 0 0 0; 0 1 0 0]. H is then [H_s 0 ... 0; 0 H_s 0 ..0; ...; ... 0 H_s]
+		H_small = Mat.zeros(2, 4, CvType.CV_64F);
+		H_small.put(0, 0, 1.0);
+		H_small.put(1, 1, 1.0);		
+		
+		H = new Mat();
+		F_small = new Mat();
+	}
+	
+	
 	public void update(ArrayList<TargetObject> targetObjects){
+		// Create combined x and P matrices from mInternalTargets 
+		
 		// Perform time update
 		
 		// Match new targetObjects with mInternalTargets by analyzing position difference and identifiers
@@ -26,17 +61,81 @@ public class Tracking {
 	
 	private void timeUpdate(){
 		// Predicted state estimate:	x 	= F*x
+		Core.gemm(F, x, 1, emptyMat, 0, x);
+		
 		// Predicted covariance:		P	= F*P*F' + Q
+		Mat tmp = new Mat();
+		Core.gemm(F.t(), P, 1, emptyMat, 0, tmp);
+		Core.gemm(tmp, F.t(), 1, Q.t(), 1, P);
 	}
 	
-	private void measurementUpdate(){
-		// Innovation of measurement: 	y	= z-H*x
+	private void measurementUpdate(Mat z){
+		// TODO make y = 0 where no measurments are found for the object
+		// Innovation of measurement: 	y	= z-H*x 			// y = 0 where we have no matching targets
+		Mat y = new Mat();
+		Core.gemm(H.t(), x, -1, z, 1, y);
+		
 		// Covariance innovation:		S	= H*P*H' + R
+		Mat S = new Mat();
+		Mat tmp = new Mat();
+		Core.gemm(H.t(), P, 1, emptyMat, 0, tmp);
+		Core.gemm(tmp, H.t(), 1, R.t(), 1, S);
+		
 		// Optimal Kalman gain: 		K	= P*H'*inv(S)
+		Mat K = new Mat();
+		tmp = new Mat();
+		Core.gemm(P.t(), H.t(), 1, emptyMat, 0, tmp);
+		Core.gemm(tmp.t(), S.inv(), 1, emptyMat, 0, K);
+		
 		// Updated estimate:			x	= x + K*y
-		// Updated covariance			P	= (I - K*H)*P
+		Core.gemm(K.t(), y, 1, x.t(), 1, x);
+		
+		// Updated covariance			P	= (I - K*H)*P = P - K*H*P
+		tmp = new Mat();
+		Core.gemm(K.t(), H, 1, emptyMat, 0, tmp);
+		Core.gemm(tmp.t(), P, -1, P, 1, P);
 	}
 	
+	private Mat createF_small(double Ts){
+		// Creates the dynamic matrix F as F_small = [1 0 Ts 0; 0 1 0 Ts; 0 0 1 0; 0 0 0 1]
+		Mat res = Mat.eye(4, 4, CvType.CV_64F);
+		res.put(0, 2, Ts);
+		res.put(1, 3, Ts);		
+		return res;
+	}
 	
+	private Mat createQ(double Ts, double sigma_sq){
+		// Creates the system noise Q = G*G'*sigma_squared
+		// Given a1 = Ts^4/4, a2 = Ts^3/2, a3 = Ts^2 we have Q as follows
+		// Q = [a1 a2 a1 a2; a2 a3 a2 a3; a1 a2 a1 a2; a2 a3 a2 a3]
+		double a1 = Math.pow(Ts, 4) / 4;
+		double a2 = Math.pow(Ts, 3) / 2;
+		double a3 = Math.pow(Ts, 2);
+		Mat res = new Mat(4, 4, CvType.CV_64F);
+		
+		res.put(0, 0, a1);
+		res.put(0, 1, a2);
+		res.put(0, 2, a1);
+		res.put(0, 3, a2);
+
+		res.put(1, 0, a2);
+		res.put(1, 1, a3);
+		res.put(1, 2, a2);
+		res.put(1, 3, a3);
+		
+		res.put(2, 0, a1);
+		res.put(2, 1, a2);
+		res.put(2, 2, a1);
+		res.put(2, 3, a2);
+
+		res.put(3, 0, a2);
+		res.put(3, 1, a3);
+		res.put(3, 2, a2);
+		res.put(3, 3, a3);
+		
+		Core.gemm(emptyMat, emptyMat, 0, res.t(), sigma_sq, res);
+		
+		return res;
+	}
 	
 }
--- src/kvaddakopter/image_processing/data_types/TargetObject.java
+++ src/kvaddakopter/image_processing/data_types/TargetObject.java
@@ -1,5 +1,49 @@
 package kvaddakopter.image_processing.data_types;
 
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
 public class TargetObject {
+	private Mat x; // State [x1, x2, dot(x1), dot(x2)]'
+	private Mat P; // Covariance
+	// private Identifier identifier; // Holds identification info about the target
 	
+	public TargetObject(Mat position_, float noise_level){
+		// Create the state matrix with given position measurements
+		x = Mat.zeros(4, 1, CvType.CV_8UC1);
+		x.put(0, 0, position_.get(0, 0));
+		x.put(1, 0, position_.get(1, 0));
+		
+		// Create the covariance matrix with noise_level on diagonal 
+		P = Mat.eye(4, 4, CvType.CV_32F);
+		P.mul(P, noise_level);
+	}
+	
+	public Mat getState(){
+		return x;
+	}
+	
+	public Mat getCovariance(){
+		return P;
+	}
+	
+	public void setState(Mat state_){
+		x = state_;
+	}
+	
+	public void setCovariance(Mat covariance_){
+		P = covariance_;
+	}
+	
+	public Mat getPosition(){
+		return x.rowRange(0, 1);
+	}
+	
+	public Mat getVelocity(){
+		return x.rowRange(2, 3);
+	}
+	
+	public double getSpeed(){
+		return Math.sqrt(Math.pow(x.get(2, 0)[0], 2) + Math.pow(x.get(3, 0)[0], 2));
+	}
 }
