69de82cafa811ce1ba1ca009f540847a3376717f
Per Öberg <per.oberg@cassun.se>
2014-11-10 10:02:06 +0100

    Added mock storage

---
+++ src/storage/MissionStorage.java
@@ -0,0 +1,28 @@
+package storage;
+
+import java.util.ArrayList;
+
+import kvaddakopter.assignment_planer.MissionObject;
+
+public class MissionStorage {
+	
+	
+	public MissionStorage(){
+		
+	}
+	
+	
+	public boolean saveMission(MissionObject mission){
+		// IMPLEMENT LOGIC TO ACTUALLY STORE A MISSION!!
+		System.out.println("Mission Saved:");
+		System.out.println(mission.toString());
+		return true;
+	}
+	
+	public ArrayList<MissionObject> getSavedMissions(){
+		ArrayList<MissionObject> missions = new ArrayList<MissionObject>();
+		
+		return missions;
+	}
+	
+}
