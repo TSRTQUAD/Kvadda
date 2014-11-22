0fe62ad485fa433e8cea948e667907ab2759ce92
Tobias Hammarling <tobiashammarling@c-208-170.eduroam.liu.se>
2014-11-22 17:29:48 +0100

    Fixed a bug in the AssignmentPlaner Matlab script and extended the GUI MockMainBus to work with the Assignmentplaner.

--- src/kvaddakopter/gui/GUITESTMain.java
+++ src/kvaddakopter/gui/GUITESTMain.java
@@ -1,14 +1,29 @@
 package kvaddakopter.gui;
 
+import matlabcontrol.MatlabConnectionException;
+import kvaddakopter.Mainbus.AssignmentPlanerRunnable;
+import kvaddakopter.assignment_planer.MatlabProxyConnection;
 import kvaddakopter.gui.interfaces.MockMainBus;
 
 public class GUITESTMain{
 
 	public static void main(String[] args) {
+		MockMainBus mainbus = new MockMainBus();
+		
+		MatlabProxyConnection proxy = new MatlabProxyConnection();
+		try {
+			proxy.startMatlab("quiet");
+		} catch (MatlabConnectionException e) {
+			System.out.println("Could not start Matlab");
+			e.printStackTrace();
+		}
+		
 		// START MODULE
-	    GUIModule gui = new GUIModule(new MockMainBus());
+	    GUIModule gui = new GUIModule(mainbus);
 	    new Thread(gui).start();
-
+	    
+	    AssignmentPlanerRunnable assignmentplaner = new AssignmentPlanerRunnable(2, mainbus);
+	    new Thread(assignmentplaner).start();
 	}
 
 }
